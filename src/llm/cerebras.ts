4f7a75792098dbe711dc6cc44031abe0db71bffc
wwwzhouhui <wwwzhouhui@163.com>
2025-08-07 12:09:49 +0800

    Update cerebras.ts

--- src/llm/cerebras.ts
+++ src/llm/cerebras.ts
@@ -2,25 +2,23 @@ import OpenAI from 'openai'
 import { openaiBase } from './openai'
 
 export function cerebras(env: Record<string, string>) {
-  // const map = {
-  //   'cerebras/llama-3.1-70b': 'llama-3-70b',
-  //   'cerebras/llama-3.1-8b': 'llama-3-8b',
-  //   'cerebras/qwen-3-235b-a22b-instruct-2507': 'qwen-3-235b-a22b-instruct-2507',
-  //   'cerebras/qwen-3-235b-a22b-thinking-2507': 'qwen-3-235b-a22b-thinking-2507',
-  //   'cerebras/qwen-3-coder-480b': 'qwen-3-coder-480b',
-  //   'cerebras/qwen-3-32b': 'qwen-3-32b',
-  //   'cerebras/gpt-oss-120b': 'gpt-oss-120b',
-  //   'cerebras/deepseek-r1-distill-llama-70b': 'cerebras/deepseek-r1-distill-llama-70b',
-  // }
+  const map = {
+    'cerebras/llama-3.1-70b': 'llama-3-70b',
+    'cerebras/llama-3.1-8b': 'llama-3-8b',
+  }
   const r = openaiBase({
     createClient: () =>
       new OpenAI({
         apiKey: env.CEREBRAS_API_KEY,
         baseURL: 'https://api.cerebras.ai/v1',
       }),
+    pre(req) {
+      req.model = map[req.model as keyof typeof map]
+      return req
+    },
   })
   r.name = 'cerebras'
   r.requiredEnv = ['CEREBRAS_API_KEY']
-  r.supportModels = ['llama-3-70b', 'llama-3-8b', 'qwen-3-235b-a22b-instruct-2507', 'qwen-3-235b-a22b-thinking-2507', 'qwen-3-coder-480b', 'qwen-3-32b', 'gpt-oss-120b', 'cerebras/deepseek-r1-distill-llama-70b']
+  r.supportModels = Object.keys(map)
   return r
 }
